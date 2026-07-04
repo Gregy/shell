aleksa-sukovic/itero-wm

commit d22d2ff059898394eb737c7dc819c7624a970644

fix: update window maximization logic to use new properties and added gnome 49 shell version in metadata

src/mod.d.ts
@@ -275,6 +275,8 @@ declare namespace Meta {
         minimized: Readonly<boolean>;
         window_type: Readonly<any>;
         decorated: Readonly<boolean>;
+        maximized_horizontally: Readonly<boolean>;
+        maximized_vertically: Readonly<boolean>;
 
         activate(time: number): void;
         change_workspace_by_index(workspace: number, append: boolean): void;
@@ -284,7 +286,6 @@ declare namespace Meta {
         get_display(): Meta.Display | null;
         get_description(): string;
         get_frame_rect(): Rectangular;
-        get_maximized(): number;
         get_monitor(): number;
         get_pid(): number;
         get_role(): null | string;

src/window.ts
@@ -276,7 +276,7 @@ export class ShellWindow {
     }
 
     is_maximized(): boolean {
-        return this.meta.get_maximized() !== 0;
+        return this.meta.maximized_horizontally || this.meta.maximized_vertically;
     }
 
     /**
@@ -300,7 +300,7 @@ export class ShellWindow {
     }
 
     is_snap_edge(): boolean {
-        return this.meta.get_maximized() == Meta.MaximizeFlags.VERTICAL;
+        return this.meta.maximized_vertically && !this.meta.maximized_horizontally;
     }
 
     is_tilable(ext: Ext): boolean {
@@ -372,9 +372,9 @@ export class ShellWindow {
         const actor = meta.get_compositor_private();
 
         if (actor) {
-            meta.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
-            meta.unmaximize(Meta.MaximizeFlags.VERTICAL);
-            meta.unmaximize(Meta.MaximizeFlags.HORIZONTAL | Meta.MaximizeFlags.VERTICAL);
+            if (this.is_maximized()) {
+                meta.unmaximize(Meta.MaximizeFlags.BOTH);
+            }
             actor.remove_all_transitions();
 
             ext.movements.insert(this.entity, clone);

src/auto_tiler.ts
@@ -700,7 +700,7 @@ export class AutoTiler {
     }
 
     private toggle_orientation_(ext: Ext, focused: ShellWindow): Result<void, string> {
-        if (focused.meta.get_maximized()) {
+        if (focused.is_maximized()) {
             return Err('cannot toggle maximized window');
         }
 

Gregy/shell

commit 08227c5acbca4fc64b3a655670cfa67b5599b1ad

src: Fix (un)maximize usage for GNOME 49.
The API had changed here:
https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4415

Thus we had warnings being logged when in stacked mode. The commit
which added support for GNOME 49 forgot to change that.

Also, to be in line with the fixes commit, we remove the
	unmaxize(HORIZONTAL);
	unmaximize(VERTICAL);
	unmaximize(BOTH);
code to checking for whether the window is maximized and then using
BOTH (i.e. its replacement) directly.

Closes: #1801
Fixes: d22d2ff05989 ("fix: update window maximization logic to use new properties and added gnome 49 shell version in metadata")
Signed-off-by: Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>

src/extension.ts
@@ -909,10 +909,10 @@ export class Ext extends Ecs.System<ExtEvent> {
             ) {
                 if (prev.rect().contains(win.rect())) {
                     if (prev.is_maximized()) {
-                        prev.meta.unmaximize(Meta.MaximizeFlags.BOTH);
+                        prev.meta.unmaximize();
                     }
                 } else if (prev.stack) {
-                    prev.meta.unmaximize(Meta.MaximizeFlags.BOTH);
+                    prev.meta.unmaximize();
                     this.auto_tiler.forest.stacks.get(prev.stack)?.restack();
                 }
             }
@@ -1034,7 +1034,7 @@ export class Ext extends Ecs.System<ExtEvent> {
                     compare.is_maximized() &&
                     win.entity[0] !== compare.entity[0]
                 ) {
-                    compare.meta.unmaximize(Meta.MaximizeFlags.BOTH);
+                    compare.meta.unmaximize();
                 }
             }
         }
@@ -1257,10 +1257,8 @@ export class Ext extends Ecs.System<ExtEvent> {
             }
 
             if (this.auto_tiler) {
-                if (this.is_floating(win)) {
-                    win.meta.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
-                    win.meta.unmaximize(Meta.MaximizeFlags.VERTICAL);
-                    win.meta.unmaximize(Meta.MaximizeFlags.BOTH);
+                if (this.is_floating(win) && win.is_maximized()) {
+                    win.meta.unmaximize();
                 }
 
                 this.register(Events.window_move(this, win, rect));
@@ -1268,7 +1266,7 @@ export class Ext extends Ecs.System<ExtEvent> {
                 win.move(this, rect, () => { });
                 // if the resulting dimensions of rect == next
                 if (rect.width == next_area.width && rect.height == next_area.height) {
-                    win.meta.maximize(Meta.MaximizeFlags.BOTH);
+                    win.meta.maximize();
                 }
             }
         }

src/mod.d.ts
@@ -306,13 +306,15 @@ declare namespace Meta {
         is_skip_taskbar(): boolean;
         make_above(): void;
         make_fullscreen(): void;
-        maximize(flags: MaximizeFlags): void;
+        maximize(): void;
+        unmaximize(): void;
+        unminimize(): void;
+        set_maximize_flags(flags: MaximizeFlags): void;
+        set_unmaximize_flags(flags: MaximizeFlags): void;
         move_frame(user_op: boolean, x: number, y: number): void;
         move_resize_frame(user_op: boolean, x: number, y: number, w: number, h: number): boolean;
         raise(): void;
         skip_taskbar: boolean;
-        unmaximize(flags: any): void;
-        unminimize(): void;
     }
 
     interface WindowActor extends Clutter.Actor {

src/tiling.ts
@@ -758,7 +758,7 @@ export class Tiler {
             this.window = win.entity;
 
             if (win.is_maximized()) {
-                win.meta.unmaximize(Meta.MaximizeFlags.BOTH);
+                win.meta.unmaximize();
             }
 
             // Set overlay to match window

src/window.ts
@@ -373,7 +373,7 @@ export class ShellWindow {
 
         if (actor) {
             if (this.is_maximized()) {
-                meta.unmaximize(Meta.MaximizeFlags.BOTH);
+                meta.unmaximize();
             }
             actor.remove_all_transitions();
 